f7693a3c7c87be65910d41b25ed347c1c4440d49
parthsaini2004 <129434562+parthsaini2004@users.noreply.github.com>
2024-01-14 12:42:32 +0530

    dashboard1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Try from './components/try/try.jsx';
 import Student from './components/student/student.jsx';
+import Data from './components/data/data.jsx'
 function App() {
   // const [showButton, setShowButton] = useState(false);
 
@@ -26,8 +27,9 @@ function App() {
    
    <Router>
    <Navbar />
+   <Data />
    {/* <Try /> */}
-   <Student />
+   {/* <Student /> */}
        {/* <div onMouseMove={handleMouseMove}>
       {showButton && <button>Click me</button> }
     </div> */}
